757ee3b7c98405b960b9631ef128d7c7970edd12
Amet Seidaliev <ametsejdaliev@gmail.com>
2023-08-23 21:39:34 +0300

    Update index.js

--- contains_duplicate_217/index.js
+++ contains_duplicate_217/index.js
@@ -2,20 +2,6 @@ const nums = [1,2,2];
 
 var containDuplicate = function (nums) {
   n = nums.length;
-  // for (let i = 0; i < n; i++) {
-  //   for (let j = 0; j < n; j++) {
-  //     j = j+1
-  //     if (nums[i] == nums[j]) {
-  //       console.log('t_nums[i]',nums[i])
-  //       console.log('t_nums[j]',nums[j])
-  //       return true;
-  //     } else {
-  //       console.log('f_nums[i]',nums[i])
-  //       console.log('f_nums[j]',nums[j])
-  //       return false;
-  //     }
-  //   }
-  //}
   let otvet;
   if (nums.length !== 1) {
     for (let i = 0; i < nums.length; i++) {
