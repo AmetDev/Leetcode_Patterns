0866cd16938578274161907da52aaf36c617a073
AmetDev <ametsejdaliev@gmail.com>
2023-08-27 16:54:57 +0300

    image-keeper-ai

---
+++ image/imageai/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "./components/Header";
+import { MainPage } from "./pages/MainPage";
+
+function App() {
+  return (
+    <>
+    <MainPage/>  
+    </>
+  );
+}
+
+export default App;
---
+++ image/imageai/src/components/GridImg.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const GridImg = () => {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4 mt-5">
+        <div className="bg-white w-[454px] h-[460px] m-2 rounded-[50px]"></div>
+        <div className="bg-white w-[454px] h-[460px] m-2 rounded-[50px]"></div>
+        <div className="bg-white w-[454px] h-[460px] m-2 rounded-[50px]"></div>
+        <div className="bg-white w-[454px] h-[460px] m-2 rounded-[50px]"></div>
+        <div className="bg-white w-[454px] h-[460px] m-2 rounded-[50px]"></div>
+        <div className="bg-white w-[454px] h-[460px] m-2 rounded-[50px]"></div>
+      </div>
+    </>
+  );
+};
+
+export default GridImg;
